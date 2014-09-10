9446bdf7b04f7d1bdaf6716080e5d9730ca8fcdf
John Qiao <qiaoshun8888@gmail.com>
2014-09-10 00:50:55 -0400

    parse sanitation schedule

--- backend/models.py
+++ backend/models.py
@@ -26,12 +26,12 @@ class Sign(models.Model):
 class LocationWithLatLng(models.Model):
     """ Locations data with latitude and longitude for each intersection.
     """
-    code = models.CharField(max_length=5, blank=False, default='')
-    status = models.CharField(max_length=15, blank=False, default='')
-    main_street = models.CharField(max_length=255, blank=False, default='')
-    from_street = models.CharField(max_length=255, blank=False, default='')
-    to_street = models.CharField(max_length=255, blank=False, default='')
-    side = models.CharField(max_length=5, blank=False, default='')
+    code = models.CharField(max_length=5, blank=True, default='')
+    status = models.CharField(max_length=15, blank=True, default='')
+    main_street = models.CharField(max_length=255, blank=True, default='')
+    from_street = models.CharField(max_length=255, blank=True, default='')
+    to_street = models.CharField(max_length=255, blank=True, default='')
+    side = models.CharField(max_length=5, blank=True, default='')
 
     # The latitude and longitude of the intersection between main_street and from_street
     lat_main_from = models.DecimalField(max_digits=15, decimal_places=8, default=0.0)
@@ -40,4 +40,3 @@ class LocationWithLatLng(models.Model):
     # The latitude and longitude of the intersection between main_street and to_street
     lat_main_to = models.DecimalField(max_digits=15, decimal_places=8, default=0.0)
     lng_main_to = models.DecimalField(max_digits=15, decimal_places=8, default=0.0)
-
--- backend/parking_finder.py
+++ backend/parking_finder.py
@@ -53,14 +53,28 @@ points (x, y) where x >= lat - T && x <= lat + T
 Use the intersection points to find all the streets/avenues
 that connect with it.
 
+Note:
+1. For a street, if one side has fire hydrants, then the other side doesn't.
+2. Valid parking place should start and end with 'Building Line' or 'Property Line'.
+3. Fire hydrant takes two parking spaces.
+4. Personal driveway takes one parking space.
+
+
+Special cases:
+,NO PARKING (SANITATION BROOM SYMBOL) 8-11AM MON & THURS SEE R7-84R
+9-10:30AM TUES & F RI W
+
 '''
+
 
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Parkers.settings")
 
+import math
 from backend.models import LocationWithLatLng, Sign
 
-delta_T = 0.0010  # 0.0065 is about 0.7 miles
+delta_T = 0.0010  # 0.0065 is about 0.7 miles.
+car_length = 20  # a car needs about 20 feet to park.
 
 
 def finder(point):
@@ -74,13 +88,62 @@ def finder(point):
     for location in locations:
         print '------------------------------------'
         print location.code, location.status, ', Main: ', location.main_street, get_code(location.code)
-        print 'From: %s (%f, %f), To: %s (%f, %f)' % (location.from_street, float(location.lat_main_from), float(location.lng_main_from),
-                                                      location.to_street, float(location.lat_main_to), float(location.lng_main_to))
-        # Get the sign info
+        print 'From: %s (%f, %f), To: %s (%f, %f), Side: %s' % (location.from_street, float(location.lat_main_from), float(location.lng_main_from),
+                                                                location.to_street, float(location.lat_main_to), float(location.lng_main_to),
+                                                                location.side)
+        # 1. Print the sign info.
+        # 2. Find the start point and end point.
         signs = Sign.objects.filter(code=location.code, status=location.status)
+        start_sign, end_sign = None, None
+        # if start_sign or end_sign is None, which means there is no 'Building Line' or 'Property Line' in sign description,
+        # then use start_sign_tmp or end_sign_tmp instead.
+        start_sign_tmp, end_sign_tmp = None, None
         for sign in signs:
-            print sign.sequence, '%5s' % sign.distance, '%5s' % sign.arrow, sign.description
+            haha(sign)
+            if ('Building Line' in sign.description or 'Property Line' in sign.description) and not start_sign:
+                start_sign = sign
+            elif ('Building Line' in sign.description or 'Property Line' in sign.description) and not end_sign:
+                end_sign = sign
+            if 'Curb Line' in sign.description:
+                if not start_sign_tmp:
+                    start_sign_tmp = sign
+                elif not end_sign_tmp:
+                    end_sign_tmp = sign
+            # print sign.sequence, '%5s' % sign.distance, '%5s' % sign.arrow, sign.description
+        if not start_sign:
+            start_sign = start_sign_tmp
+        if not end_sign:
+            end_sign = end_sign_tmp
+        if start_sign and end_sign:
+            print 'Estimated parking slots: ', estimate_slots(end_sign.distance - start_sign.distance)
         print ''
+
+
+def haha(sign):
+    """Get sanitation schedule.
+    """
+    import re
+    m = re.findall(r'(\d{1,2}:?\d{0,2}((A|P)M)?)(-|( TO ))(\d{1,2}:?\d{0,2}((A|P)M)?)\s([\w\ \&]*\w+)\ ', sign.description)
+    if m:
+        m = m[0]
+        print m[0], m[5], m[8]
+
+
+def estimate_slots(length):
+    return int(math.floor(length / car_length))
+
+
+def get_sanitation_schedule(signs):
+    """
+    :param signs: a list of signs of the street.
+    :return:
+    """
+    start_sign, end_sign = None, None
+    for (index, sign) in enumerate(signs):
+        if 'SANITATION BROOM SYMBOL' in sign.description and not start_sign:
+            start_sign = sign
+        if 'SANITATION BROOM SYMBOL' in sign.description:
+            end_sign = sign
 
 
 def get_code(v):
