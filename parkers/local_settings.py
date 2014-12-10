65502028f93af208d055ba910fc7fe06024390de
John Qiao <qiaoshun8888@gmail.com>
2014-12-10 12:11:18 -0500

    paper in course CS-GY-6015, Fall 2014

--- parkers/local_settings.py
+++ parkers/local_settings.py
@@ -5,3 +5,14 @@ Created on 9/5/14
 
 @author: 'johnqiao'
 """
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'parkers',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
